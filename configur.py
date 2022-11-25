9460959e07a6a59b329b8cc83e061095b5004b08
AnnaZABARA <zabara.anna8@gmail.com>
2022-11-25 13:59:58 +0300

    Update

--- configur.py
+++ configur.py
@@ -1,3 +1,3 @@
-TOKEN = '5897364507:AAEvh3LgZIOOFqBfnh51Ws9L_H6qFcqgkHo'
+TOKEN = '5897364507:AAEvh3LgZIOOFqBfnh51Ws9L_H6qFcqgkHo' # Токен, полученный из BotFather Телеграм, при создании нового бота
 
-keys = {"евро": 'EUR', "рубль": 'RUB', 'доллар': "USD", }
+keys = {"евро": 'EUR', "рубль": 'RUB', 'доллар': "USD", } # Словарь с возможными вариантами валют для конвертации
