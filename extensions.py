bfc96b37fef63af33022246615bb0dd42c242ee9
AnnaZABARA <zabara.anna8@gmail.com>
2022-11-25 14:01:08 +0300

    Update

--- extensions.py
+++ extensions.py
@@ -1,12 +1,12 @@
-import requests
+import requests # импортируем бибилиотеки для работы с API и json
 import json
-from configur import keys
+from configur import keys #импортируем из файла словарь для работы с классами
 
 
-class CurrencyException(Exception):
+class CurrencyException(Exception): # добавляем класс искллючений
     pass
 
-class CurrencyConverter:
+class CurrencyConverter: #добавляем класс исключений и прописываем возможные ошибки со стороны сервера и со стороны неверного ввода пользователя, например при вводе одинаковых валют
     @staticmethod
     def get_price(quote: str, base:str, amount:str):
         if quote == base:
@@ -27,7 +27,7 @@ class CurrencyConverter:
         except KeyError:
             raise CurrencyException(f'Не удалось обработать количество {amount}')
 
-        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')#ключ API  с сайта, для конвертации по текущему курсу
         total_base = json.loads(r.content)[keys[base]]
 
-        return total_base*amount
+        return total_base*amount # возвращаем общее количество валюты
